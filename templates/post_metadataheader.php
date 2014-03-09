ee9f0764584caf209cdefc98ae71e8507f552bf4
Victoria Holland <victoriaholland1984@hotmail.com>
2014-03-09 16:49:56 +0000

    PartyTime WordPress theme

---
+++ templates/post_metadataheader.php
@@ -0,0 +1,6 @@
+                                        <div class="art-postmetadataheader">
+                                            <?php echo $post_title; ?>
+                                              <div class="art-postheadericons art-metadata-icons">
+                                                <?php echo $postheadericons; ?>    
+                                            </div>
+                                        </div>
---
+++ templates/post_title.php
@@ -0,0 +1,5 @@
+                                            <h2 class="art-postheader">
+                                              <img src="<?php echo $template_url; ?>/images/postheadericon.png" width="28" height="27" alt="" />
+                                              <a href="<?php echo $post_link; ?>" rel="bookmark" title="<?php echo $post_link_title; ?>">
+                                               <?php echo $post_title; ?>
+                                              </a></h2>
